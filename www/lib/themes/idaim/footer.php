357412fed778bf554974c95d556527f65f3368ea
Carlos Hugo Gonzalez Castell <caarloshugo@gmail.com>
2012-11-05 22:51:18 -0600

    arreglando el diseño del home 960px

--- www/lib/themes/idaim/footer.php
+++ www/lib/themes/idaim/footer.php
@@ -1,5 +1,5 @@
 		<div id="footer">
-			<a class="logos-footer fundar-logo margin-left" href="http://fundar.org.mx" title="Fundar, Centro de Análisis e Investigación">Fundar, Centro de An&aacute;lisis e Investigaci&oacute;n</a>
+			<a class="logos-footer fundar-logo" href="http://fundar.org.mx" title="Fundar, Centro de Análisis e Investigación">Fundar, Centro de An&aacute;lisis e Investigaci&oacute;n</a>
 			<a class="logos-footer article19-logo" href="http://www.article19.org" title="Article 19">Article 19</a>
 			<p>
 				Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
